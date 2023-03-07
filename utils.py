e9d9b5ce09a9c55c2ed0030ffe3ed38ca970d2f3
Eloi Campagne <eloi.campagne@gmail.com>
2023-03-07 14:42:43 +0100

    feat: extract test bases + grad loss with torch + plot results

--- utils.py
+++ utils.py
@@ -1,38 +1,40 @@
 import numpy as np
-import pandas as pd
+from tqdm import tqdm
+import torch
+
 
 class Transfer():
-    def __init__(self, betas, betas_it, basis, basis_it, y, y_it, K=30):
+    def __init__(self, betas, betas_it, basis, basis_it, y, y_it, K=75):
         # Coefficients learned on the French source data
         self.betas = betas.copy()
-        self.betas_vec = np.array([betas.to_numpy()[t][1] for t in range(len(betas))])
+        self.betas_vec = betas['betas_france'].to_numpy()
         # Coefficients learned on the Italian source data
         self.betas_it = betas_it.copy()
-        self.betas_it_vec = np.array([betas_it.to_numpy()[t][1] for t in range(len(betas_it))])
+        self.betas_it_vec = betas_it['betas_italy'].to_numpy()
         # Spline basis for the French source data
-        self.basis = basis[:,1:]
+        self.basis = basis.to_numpy()[:, 1:]
         # Spline basis for the Italian source data
-        self.basis_it = basis_it[:,1:]
+        self.basis_it = basis_it.to_numpy()[:, 1:]
         # Number of iterations
         self.K = K
-        # Target data (French)
+        #  Target data (French)
         self.y = y
-        # Target data (Italian)
+        #  Target data (Italian)
         self.y_it = y_it
-        # Scale parameter (should be around 4)
+        #  Scale parameter (should be around 4)
         self.rho = y.sum() / y_it.sum()
 
-    def compute_alpha(self, country='fr'):
+    def compute_alpha(self, t, country='fr'):
         if country == 'fr':
             basis = self.basis
         else:
             basis = self.basis_it
-        lam_max = np.linalg.eig(basis.T@basis)[0].max()
-        lam_min = np.linalg.eig(basis.T@basis)[0].min()
-        return .4/(lam_max + lam_min)
-    
+        lam_max = np.real(np.linalg.eig(basis[:t].T@basis[:t])[0]).max()
+        lam_min = np.real(np.linalg.eig(basis[:t].T@basis[:t])[0]).min()
+        return .4/(lam_max + lam_min + 1e-8)
+
     def grad_loss(self, t, country='fr'):
-        self.alpha = self.compute_alpha(country)
+        self.alpha = self.compute_alpha(t, country)
         if country == 'fr':
             y = self.y
             beta = self.betas_vec
@@ -41,24 +43,37 @@ class Transfer():
             y = self.y_it
             beta = self.betas_it_vec
             basis = self.basis_it
-        obj = 0
-        for s in range(t-1):
-            obj += self.alpha*(-2)*(y[s] - beta.T@basis[s])*basis[s]
-        return obj
-    
+        beta_te = torch.tensor(beta, requires_grad=True)
+        y_te = torch.tensor(y[:-t])
+        basis_te = torch.tensor(basis[:-t].T)
+        # Compute the loss
+        loss = torch.sum((y_te - beta_te@basis_te)**2)
+        # Compute the gradient
+        loss.backward()
+        # Return the gradient to numpy
+        return self.alpha*beta_te.grad.numpy()
+
     def gam_ft(self):
         beta_t = self.betas_vec
-        for t in range(self.K):
-            beta_t = beta_t - self.grad_loss(t)
+        for k in range(self.K):
+            beta_t = beta_t - self.grad_loss(k)
         return beta_t@self.basis.T
-    
+
+    # def gam_ft(self, type='test1'):
+    #     if type == 'test1':
+    #         l = len(test1)
+    #     elif type == 'test2':
+    #         l = len(test2)
+    #     tuning = []
+    #     for t in tqdm(range(l)):
+    #         tuning.append(self.gam_ft_t(t))
+    #     return tuning
+
     def gam_delta(self):
+        # NOT WORKING
         beta_t_it = self.betas_it_vec
         for t in range(self.K):
             beta_t_it = beta_t_it - self.grad_loss(t, country='it')
-        self.betas_it['betas_italy'] = beta_t_it
-        df_same = pd.merge(self.betas, self.betas_it)
-        delta_t = df_same['betas_italy'].to_numpy() - df_same['betas_france'].to_numpy()
-        df_same['betas_france'] += self.rho*delta_t
-        self.betas.update(df_same['betas_france'])
-        return self.betas['betas_france'].to_numpy()@self.basis.T
+        delta_t = beta_t_it - self.betas_it_vec
+        beta_t = self.betas_vec + self.rho*delta_t
+        return beta_t.T@self.basis
