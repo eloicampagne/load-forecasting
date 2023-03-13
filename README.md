f5d91c325997826709b6cb24c87a3ba6cf3010d4
Eloi Campagne <eloi.campagne@gmail.com>
2023-03-14 00:45:36 +0100

    feat: update gam, implement gam saturday and aggregate, docs and plots

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The coronavirus disease 2019 (COVID-19) pandemic has caused significant changes 
 
 This project aims to reproduce a solution developed in [this paper](https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=9382417) to the issue of poor performances exhibited by traditional electricity load forecasting models since the beginning of the pandemic of COVID-19. These models are trained on historical data and rely on calendar or meteorological information.
 
-The solution involves two methods: Kalman filters and fine-tuning. Kalman filters are used to model the dynamic behavior of the electricity load during lockdown periods, while fine-tuning allows the model to quickly adapt to new consumption patterns without requiring exogenous information.
+We are only focusing on fine-tuning which allows the model to quickly adapt to new consumption patterns without requiring exogenous information.
 
 The developed models are applied to forecast the electricity demand during the French lockdown period, and expert aggregation is used to leverage the specificities of each prediction and enhance results even further.
 
@@ -14,16 +14,74 @@ The developed models are applied to forecast the electricity demand during the F
 
 The data used in this project is publicly available and can be found in the data directory. 
 
-![data](https://user-images.githubusercontent.com/78951592/221935428-efa812bb-55ca-4f6e-8fcf-2d0729425664.png)
 
 * `Température` : temperature in Celcius
 * `Temp95` and `Temp99` : exponentially smoothed temperatures of factors .95 and .99
 * `TempMin99` and `TempMax99` : minimal and maximal value of `Temp99` at the current day
 * `Consommation` : electricity consumption in MW
 * `Consommation1` and `Consommation7` : consumptions of the day before and the week before
+* `DateN` : number of the day since the beginning of the dataset
 * `TimeOfYear` : time of year (0 = 1st of January at 00:00, 1 = 31st of December at 23:30)
 * `DayType` : categorical variable indicating the type of the day (0 = Monday, 6 = Sunday)
 * `DLS` : binary variable indicating whether it is summertime or not
+
+## Getting started
+
+*We chose to develop this project in both Python and R to get the best out of the already developed libraries.*
+
+### Prerequisites
+
+Before you start, make sure you have the following packages installed on Python :
+
+* `numpy`
+* `pandas`
+* `torch`
+* `tqdm`
+
+and on R :
+* `opera`
+* `mgcv`
+* `caret`
+* `riem`
+
+### Cloning the repository
+
+To clone the repository, run the following command in your terminal 
+
+```
+git clone git@github.com:Exion35/load-forecasting.git
+```
+
+or
+
+```
+git clone https://github.com/Exion35/load-forecasting.git
+```
+
+## Navigating through the repository
+
+1. Get the Italian weather data with `get_it_weather.ipynb`
+2. Process both French and Italian data with `preprocessing.ipynb`
+3. Build the experts (GAM, GAM Saturday, GBM) with `build_experts.ipynb`
+4. Fine-tune the GAM and display the results with `fine_tuning_gam.ipynb`
+5. Aggregate the experts with `aggregate_experts.ipynb` (you can come back to `fine_tuning_gam.ipynb` to display the aggregation plot)
+
+## Results
+
+| Method                       | Test 1  | Test 2   |
+|:----------------------------:|:-------:|:--------:|
+| GAM                          |5.40%    |3.77%     |
+| GBM                          |6.34%    |5.04%     |
+| Fine-tuned                   |3.96%    |3.78%     |
+| GAM $\delta$                 |24.15 (!)|6.45%     |
+| GAM $\delta$ - Fine-tuned    |-        |-         |   
+| GAM Saturday                 |4.02%    |5.78%     |
+| Aggregation with GAM Saturday|**2.80%**|**3.15%** |
+
+
+
+
+
 
 ## References
 
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import torch
 
 class Transfer():
 
-    def __init__(self, betas_fr, betas_it, basis_fr, basis_it, y_fr, y_it,rho = None, K=75):
+    def __init__(self, betas_fr, betas_it, basis_fr, basis_it, y_fr, y_it, rho=None, K=75):
         '''
         Performs the transfer learning algorithm.
  
@@ -19,7 +19,8 @@ class Transfer():
             K : int, number of iterations for the gradient descent in the finetuning step
         '''
 
-        common_idx,fr_to_it, it_to_fr =  np.intersect1d(betas_it.index, betas_fr.index , return_indices=True)
+        common_idx, fr_to_it, it_to_fr = np.intersect1d(
+            betas_it.index, betas_fr.index, return_indices=True)
         self.fr_to_it = fr_to_it
         self.it_to_fr = it_to_fr
 
@@ -44,7 +45,6 @@ class Transfer():
         else:
             self.rho = rho
 
-        
     def compute_alpha(self, t, country='fr'):
         """
         Computes the step size alpha at time t
@@ -63,8 +63,7 @@ class Transfer():
         lam_min = np.real(np.linalg.eig(basis[:t].T@basis[:t])[0]).min()
         return .4/(lam_max + lam_min + 1e-8)
 
-
-    def gradient_descent(self,beta, y, basis, t):
+    def gradient_descent(self, beta, y, basis, t):
         """
         Computes the gradient descent at time t given the initial coefficients beta.
         Input:
@@ -75,17 +74,18 @@ class Transfer():
         Returns:
             beta_t: np.array, coefficients at time t
         """
-        # Convert all the numpy arrays to torch tensors
+        #  Convert all the numpy arrays to torch tensors
         beta_te = torch.tensor(beta, requires_grad=True)
-        y_te = torch.tensor(y[:t], requires_grad=False) # No data leakage. We only use the data up to time t
+        # No data leakage. We only use the data up to time t
+        y_te = torch.tensor(y[:t], requires_grad=False)
         basis_te = torch.tensor(basis[:t].T, requires_grad=False)
 
-        # Define the optimizer (SGD)
+        #  Define the optimizer (SGD)
         # With one parameter it amounts to compute the vanilla gradient descent
         optimizer = torch.optim.SGD([beta_te], lr=self.alpha)
 
         for k in range(self.K):
-            # Zero the parameter gradients	
+            # Zero the parameter gradients
             optimizer.zero_grad()
             # Compute the loss
             loss = torch.sum((y_te - beta_te@basis_te)**2)
@@ -93,13 +93,13 @@ class Transfer():
             loss.backward()
             # Backpropagate the gradient
             optimizer.step()
-        
-        # Convert the torch tensor to numpy array
+
+        #  Convert the torch tensor to numpy array
         beta_t = beta_te.detach().numpy()
 
         return beta_t
-    
-    def gam_ft_t(self, t,country='fr'):
+
+    def gam_ft_t(self, t, country='fr'):
         """
         Computes GAM finetuning at time t.
         It finds the best coefficients beta_t to predict up to y_t.
@@ -147,14 +147,13 @@ class Transfer():
 
         tuning = []
         for t in tqdm(range(l)):
-            # Compute the finetuning at time t
-            beta_t = self.gam_ft_t(t,country=country)
-            # Compute the prediction at time t
-            # tuning.append(betas@basis[t].T)
+            #  Compute the finetuning at time t
+            beta_t = self.gam_ft_t(t, country=country)
+            #  Compute the prediction at time t
             tuning.append(beta_t@basis[t].T)
         return tuning
 
-    def gam_delta_t(self,t,country_orig='it'):
+    def gam_delta_t(self, t, country_orig='it'):
         '''
         Compute the delta coefficients and the beta coefficient for the transfer learning algorithm.
         Corresponds to steps 1, 2, 3 of GAM-delta.
@@ -165,7 +164,7 @@ class Transfer():
         Returns:
             beta_array_dst: np.array, coefficients of the destination country
         '''
-        beta_src_t = self.gam_ft_t(t,country=country_orig)
+        beta_src_t = self.gam_ft_t(t, country=country_orig)
 
         if country_orig == 'fr':
             beta_0_src = self.betas_fr_0
@@ -178,7 +177,7 @@ class Transfer():
 
         beta_dst_t = np.copy(beta_0_dst)
         delta_t = beta_src_t - beta_0_src
-        
+
         if country_dest == 'fr':
             beta_dst_t[self.it_to_fr] += self.rho*delta_t[self.it_to_fr]
         else:
@@ -186,8 +185,7 @@ class Transfer():
 
         return beta_dst_t
 
-
-    def gam_delta(self,country_orig='it', country_dst='fr'):
+    def gam_delta(self, country_orig='it', country_dst='fr'):
         '''
         Compute the prediction of the transfer learning algorithm, ie step 4 of GAM-delta.
 
@@ -207,13 +205,12 @@ class Transfer():
 
         tuning = []
         for t in tqdm(range(l)):
-            # Compute the prediction at time t
-            beta_t = self.gam_delta_t(t,country_orig=country_orig)
+            #  Compute the prediction at time t
+            beta_t = self.gam_delta_t(t, country_orig=country_orig)
             tuning.append(beta_t@basis[t].T)
         return tuning
-    
 
-    def gam_delta_ft_t(self,t,country_src='it',country_dst='fr'):
+    def gam_delta_ft_t(self, t, country_src='it', country_dst='fr'):
         '''
         Performs the finetuning of the transfer learning algorithm at time t.
 
@@ -232,11 +229,11 @@ class Transfer():
             y = self.y_it
             basis = self.basis_it
 
-        beta_t = self.gam_delta_t(t,country_orig=country_src)
+        beta_t = self.gam_delta_t(t, country_orig=country_src)
         beta_t = self.gradient_descent(beta_t.copy(), y, basis, t)
         return beta_t
-    
-    def gam_delta_ft(self,country_src='it',country_dst='fr'):
+
+    def gam_delta_ft(self, country_src='it', country_dst='fr'):
         '''
         Performs the prediction of the finetuning of the transfer learning algorithm for all time t.
 
@@ -255,8 +252,9 @@ class Transfer():
 
         tuning = []
         for t in tqdm(range(l)):
-            # Compute the prediction at time t
-            beta_t = self.gam_delta_ft_t(t,country_src=country_src,country_dst=country_dst)
+            #  Compute the prediction at time t
+            beta_t = self.gam_delta_ft_t(
+                t, country_src=country_src, country_dst=country_dst)
             tuning.append(beta_t@basis[t].T)
         return tuning
 
@@ -273,3 +271,16 @@ def mape(y_true, y_pred):
         mape: float, mean absolute percentage error
     '''
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+def rmse(y_true, y_pred):
+    '''
+    Computes the root mean squared error
+
+    Input:
+        y_true: np.array, true values
+        y_pred: np.array, predicted values
+
+    Returns:
+        rmse: float, root mean squared error
+    '''
+    return np.sqrt(np.mean((y_true - y_pred)**2))
